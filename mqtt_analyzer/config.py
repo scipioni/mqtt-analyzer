9ff45e194eebd0dda1288fa918b17b62e06573ca
Stefano Scipioni <stefano.scipioni@csgalileo.org>
2023-02-06 11:52:07 +0100

    save config

--- mqtt_analyzer/config.py
+++ mqtt_analyzer/config.py
@@ -9,4 +9,14 @@ def get_config() -> Any:
     parser.add_argument("--port", default=1883, type=int)
     parser.add_argument("--out", default="/tmp/data.csv")
     parser.add_argument("--test", action="store_true", default=False)
+    parser.add_argument(
+        "-c", "--config", required=False, is_config_file=True, help="config file path"
+    )
+    parser.add_argument(
+        "--config-save",
+        required=False,
+        is_write_out_config_file_arg=True,
+        help="config file path",
+    )
+
     return parser.parse_args()
